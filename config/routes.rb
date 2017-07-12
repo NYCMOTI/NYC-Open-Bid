ac1012976c8df9a0da5d0540fc67c0ad83de8647
Jason Jeffries <jjeffries@blenderbox.com>
2017-07-12 00:00:00 -0400

    updater

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get '/admin', to: 'admin/auctions/needs_attention#index'
   get '/sign_up', to: 'sign_ups#show'
   get '/sign_in', to: 'sign_ins#show'
+  get '/update', to: 'update#pull'
 
   #get '/api' => 'docs#index', as: 'api_doc'
 
